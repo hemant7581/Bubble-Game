2f85c05ef9ccfd5230fe445c8541f3e68b42655c
hemant7581 <87279045+hemant7581@users.noreply.github.com>
2023-08-18 00:29:01 +0530

    Add files via upload

---
+++ Bubble-Game/Index.js
@@ -0,0 +1,56 @@
+let Hit = 0;
+
+//function for creating multiple bubbles starts here
+function makeBubble(){
+    
+    //for storing value 
+    let clutter = ""
+    //for creating multiple bubbles we use for loop
+    for (let i = 0; i <=210; i++) {
+        //generating random numbers
+        let num =Math.floor(Math.random()*10)
+        clutter +=`<div class="bubble">${num}</div>`
+        
+    }
+    document.querySelector('.bottom-bar').innerHTML=clutter;
+}
+
+let timer = 60;
+//function for runtimer
+function runtimer() {
+    let timerInt = setInterval(function(
+      
+        ) {
+            if(timer>0){
+                timer--;
+                document.querySelector('#timerVal').textContent = timer; 
+            }
+            else(
+                clearInterval(timerInt)
+                )  
+            }, 1000);
+        }
+        
+//function for hitVal
+function hitVal() {
+    Hit = Math.floor(Math.random()*10);
+    document.querySelector('#HitVal').textContent= Hit;
+}
+
+let score = 0
+timer = 60;
+//function for IncScore
+function IncScore() {
+    score += 10;
+    document.querySelector('#scoreVal').textContent=score;
+}
+
+document.querySelector('.bottom-bar').addEventListener('click', function(details){
+    let clickedNum = Number(details.target.textContent);
+    if (clickedNum == Hit) {
+        IncScore();
+    }
+});
+runtimer();
+makeBubble();
+hitVal();
---
+++ Bubble-Game/readme.md
@@ -0,0 +1,26 @@
+# Bubble Game - Understanding Event Bubbling in JavaScript
+
+[Bubble Game Screenshot](screenshot.png)
+
+This project is a simple Bubble game created using HTML, CSS, and JavaScript. Its main purpose is to provide a hands-on demonstration of the concept of event bubbling in the context of Web development.
+
+## Getting Started
+
+To run the Bubble game locally, follow these steps:
+
+1. Clone this repository:
+
+   ```sh
+   git clone git@github.com:hemant7581/Bubble-Game.git
+   ```
+
+## Event Bubbling
+
+Event bubbling is a concept in web development that relates to how events are handled and propagated through the Document Object Model (DOM) hierarchy in HTML. When an event, such as a click or keypress, occurs on an HTML element, it not only triggers the event handler associated with that element but also propagates through the ancestors of that element in the DOM tree.
+
+## Features
+
+- Interactive bubbles that respond to clicks.
+- Real-time visualization of event bubbling.
+- Simple and intuitive user interface.
+
